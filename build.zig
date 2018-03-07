d760aa546ea632e3ac464d836f598c1046716eda
Marc Tiehuis <marctiehuis@gmail.com>
2018-03-07 18:10:04 +1300

    Improve build command for running all tests

--- build.zig
+++ build.zig
@@ -4,20 +4,9 @@ pub fn build(b: &Builder) void {
     const mode = b.standardReleaseOptions();
 
     const test_step = b.step("test", "Run all tests");
-
-    const test_files = [][]const u8 {
-        "parse_test.zig",
-        "exec_test.zig",
-        "regex_test.zig",
-    };
-
-    inline for (test_files) |test_file| {
-        const this_test = b.addTest("src/" ++ test_file);
-        test_step.dependOn(&this_test.step);
-    }
+    test_step.dependOn(&b.addTest("src/all_test.zig").step);
 
     const build_lib_step = b.step("library", "Build static library");
-
     const build_lib = b.addStaticLibrary("regex", "src/regex.zig");
     build_lib_step.dependOn(&build_lib.step);
 
