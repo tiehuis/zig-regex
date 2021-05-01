bf841fd47db90b8a1e2f37a5b355d30bcd60274c
joachimschmidt557 <joachim.schmidt557@outlook.com>
2021-05-01 14:21:42 +0800

    Update c_regex.zig to latest master and provide an example for interop with C API

--- build.zig
+++ build.zig
@@ -7,8 +7,17 @@ pub fn build(b: *Builder) void {
     test_step.dependOn(&b.addTest("src/all_test.zig").step);
 
     const build_lib_step = b.step("library", "Build static library");
-    const build_lib = b.addStaticLibrary("regex", "src/regex.zig");
+    const build_lib = b.addStaticLibrary("regex", "src/c_regex.zig");
+    build_lib.linkLibC();
     build_lib_step.dependOn(&build_lib.step);
+
+    const c_example_step = b.step("c-example", "Example using C API");
+    const c_example = b.addExecutable("example", "example/example.c");
+    c_example.addIncludeDir("include");
+    c_example.linkLibC();
+    c_example.linkLibrary(build_lib);
+    c_example_step.dependOn(&build_lib.step);
+    c_example_step.dependOn(&c_example.step);
 
     b.default_step.dependOn(test_step);
 }
