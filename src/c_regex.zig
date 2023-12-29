20ae38e105bb387b433672f1669fa443f22a90d9
Marc Tiehuis <marc@tiehu.is>
2023-12-30 10:47:40 +1300

    update for 0.11 @ptrCast/@alignCast syntax change

--- src/c_regex.zig
+++ src/c_regex.zig
@@ -19,40 +19,39 @@ var allocator = std.heap.c_allocator;
 export fn zre_compile(input: ?[*:0]const u8) ?*zre_regex {
     var r = allocator.create(Regex) catch return null;
     r.* = Regex.compile(allocator, std.mem.span(input.?)) catch return null;
-    return @ptrCast(?*zre_regex, r);
+    return @ptrCast(r);
 }
 
 export fn zre_match(re: ?*zre_regex, input: ?[*:0]const u8) bool {
-    var r = @ptrCast(*Regex, @alignCast(8, re));
+    var r: *Regex = @ptrCast(@alignCast(re));
     return r.match(std.mem.span(input.?)) catch return false;
 }
 
 export fn zre_partial_match(re: ?*zre_regex, input: ?[*:0]const u8) bool {
-    var r = @ptrCast(*Regex, @alignCast(8, re));
+    var r: *Regex = @ptrCast(@alignCast(re));
     return r.partialMatch(std.mem.span(input.?)) catch return false;
 }
 
 export fn zre_deinit(re: ?*zre_regex) void {
-    var r = @ptrCast(*Regex, @alignCast(8, re));
+    var r: *Regex = @ptrCast(@alignCast(re));
     r.deinit();
     allocator.destroy(r);
 }
 
 export fn zre_captures_all(re: ?*zre_regex, input: ?[*:0]const u8) ?*zre_captures {
-    var r = @ptrCast(*Regex, @alignCast(8, re));
+    var r: *Regex = @ptrCast(@alignCast(re));
     var c = allocator.create(Captures) catch return null;
     c.* = (r.captures(std.mem.span(input.?)) catch return null) orelse return null;
-    return @ptrCast(?*zre_captures, c);
-    
+    return @ptrCast(c);
 }
 
 export fn zre_captures_len(cap: ?*const zre_captures) usize {
-    const c = @ptrCast(*const Captures, @alignCast(8, cap));
+    const c: *const Captures = @ptrCast(@alignCast(cap));
     return c.slots.len / 2;
 }
 
 export fn zre_captures_slice_at(cap: ?*const zre_captures, n: usize, len: ?*usize) ?[*]const u8 {
-    const c = @ptrCast(*const Captures, @alignCast(8, cap));
+    const c: *const Captures = @ptrCast(@alignCast(cap));
     var slice = c.sliceAt(n) orelse return null;
     if (len) |ln| {
         ln.* = slice.len;
@@ -61,9 +60,9 @@ export fn zre_captures_slice_at(cap: ?*const zre_captures, n: usize, len: ?*usiz
 }
 
 export fn zre_captures_bounds_at(cap: ?*const zre_captures, sp: ?*zre_captures_span, n: usize) bool {
-    const c = @ptrCast(*const Captures, @alignCast(8, cap));
+    const c: *const Captures = @ptrCast(@alignCast(cap));
     var span = c.boundsAt(n);
-    if (span) |s| {      
+    if (span) |s| {
         sp.?.*.lower = s.lower;
         sp.?.*.upper = s.upper;
         return true;
@@ -72,7 +71,7 @@ export fn zre_captures_bounds_at(cap: ?*const zre_captures, sp: ?*zre_captures_s
 }
 
 export fn zre_captures_deinit(cap: ?*zre_captures) void {
-    var c = @ptrCast(*Captures, @alignCast(8, cap));
+    var c: *Captures = @ptrCast(@alignCast(cap));
     c.deinit();
     allocator.destroy(c);
 }
